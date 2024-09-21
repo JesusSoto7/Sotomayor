62ccf475118d13db1b412f8e4987cbb72377d094
JesusSoto7 <jesusmsotor07@gmail.com>
2024-09-20 20:58:32 -0500

    Todo listo por ahora

--- jesus_sotomayor_rodriguez/POO/Peticiones/actividad20-09.dart
+++ jesus_sotomayor_rodriguez/POO/Peticiones/actividad20-09.dart
@@ -4,9 +4,9 @@ import 'dart:convert';
 class Agua{
     int? exp;
 
-    Agua(Map jsonString){
+    Agua(String jsonString){
         Map<String, dynamic> map = jsonDecode(jsonString);
-        this.exp = map['base_experience:'];
+        this.exp = map['ability'];
     }
 }
 
@@ -14,8 +14,6 @@ void main() async {
   var url = Uri.https('pokeapi.co', 'api/v2/pokemon/greninja');
   var response = await http.get(url);
   print('Response status: ${response.statusCode}');
-  var greninja = '${response.body}';
-
-  Map<String, dynamic> map = jsonDecode(greninja);
-  print(map['forms'][0]['name']);
+  Agua agua = Agua(response.body);
+  print(agua.exp);
 }
