1ad350fb91bf33fb347e306713ade3e358c523e9
JesusSoto7 <jesusmsotor07@gmail.com>
2024-10-21 13:01:44 -0500

    aplicacion sencilla

--- jesus_sotomayor_rodriguez/POO/Peticiones/Flutter/flutter_application_1/lib/main.dart
+++ jesus_sotomayor_rodriguez/POO/Peticiones/Flutter/flutter_application_1/lib/main.dart
@@ -7,56 +7,82 @@ void main() {
   runApp(MyApp());
 }
 
-Future<User> fetchData() async {
-  var url = Uri.https('jsonplaceholder.typicode.com', 'users/9');
+Future<User> fetchData(int id) async {
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users/${id}');
   var response = await http.get(url);
   return User(response.body);
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  MyAppful createState() => MyAppful();
+}
+
+class MyAppful extends State<MyApp> {
+  final TextEditingController controldebarra = TextEditingController();
+  int userId = 1;
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PlaceHolder',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('User')),
-        body: FutureBuilder<User>(
-          future: fetchData(),
-          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (snapshot.hasData) {
-              User user = snapshot.data!;
-              return Center(
-                child: Column(
-                  children: [
-                    Text('ID: ${user.id}'),
-                    Text('Name: ${user.name}'),
-                    Text('Username: ${user.username}'),
-                    Text('Email: ${user.email}'),
-                    Text('Address: ${user.address!.city!}'),
-                    Text('Geo: Lat ${user.address!.geo!.lat!}'),
-                    Text('Phone: ${user.phone}'),
-                    Text('Website: ${user.website}'),
-                    Text('Company: ${user.company!.name!}'),
-                    //ElevatedButton(
-                    //onPressed: () {}, child: Text('Buscar USER')),
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.green,
-                      size: 24.0,
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return Center(child: Text('No data available'));
-            }
-          },
+        title: 'PlaceHolder',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Text('User')),
+
+          body: Column(
+            children: [
+              TextField(
+                controller: controldebarra,
+                decoration: InputDecoration(labelText: 'Ingresa Id' ),
+                keyboardType: TextInputType.number,
+              ),
+              
+              ElevatedButton(
+                onPressed: () {
+                  int idIngresado = int.tryParse(controldebarra.text) ?? 0;
+                  if (idIngresado > 0) {
+                    setState(() {
+                      userId = idIngresado;
+                    });
+                  }
+                },
+                child: Text('Buscar USER'),
+              ),
+              FutureBuilder<User> (
+                future: fetchData(userId!),
+                builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (snapshot.hasData) {
+                    User user = snapshot.data!;
+                    return Center(
+                      child: Column(
+                        children: [
+                          Text('ID: ${user.id}'),
+                          Text('Name: ${user.name}'),
+                          Text('Username: ${user.username}'),
+                          Text('Email: ${user.email}'),
+                          Text('Address: ${user.address!.city!}'),
+                          Text('Geo: Lat ${user.address!.geo!.lat!}'),
+                          Text('Phone: ${user.phone}'),
+                          Text('Website: ${user.website}'),
+                          Text('Company: ${user.company!.name!}'),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.green,
+                            size: 24.0,
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                  return Center(child: Text('No data available'));
+                  }
+                },
+              ),
+          ]),
         ),
-      ),
     );
   }
 }
